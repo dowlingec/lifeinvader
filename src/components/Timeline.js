1655f6488b1994f96753ecb5f9be07320c66730c
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-07 18:01:50 -0400

    Added posts request

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,19 @@
-const Timeline = ({  }) => {
+import { useEffect, useState } from 'react'
+
+const Timeline = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(async () => {
+        console.log('renderingTimline')
+        let req = await fetch(`https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json`)
+        let res = await req.json()
+        setPosts(res)
+    }, [])
+
     return(
         <div>
             <h2>Timeline</h2>
+            
         </div>
     )
 }
