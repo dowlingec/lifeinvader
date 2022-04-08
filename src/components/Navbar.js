3dcbd9c4e2fca0b9a511922cc7f17c85129c027d
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-08 14:40:11 -0400

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 const Navbar = () => {
     return (
-        <nav classNAme="navbar">
+        <nav className="navbar">
             <div>
-                <a href="#">Lifeinvader</a>
+                <a href="#" className="home-link">Lifeinvader</a>
             </div>
             <div>
                 <i class="fa-solid fa-magnifying-glass"></i>
