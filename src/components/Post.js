e346179ba776e26afa96b0d964773fbfece223a1
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-08 13:44:06 -0400

    addedlike button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
+
 const Post = ({ post }) => {
+    const [likes, setLikes] = useState(0)
+    
     return (
         <div>
             <img src={post.image} height="250" width="250"></img>
-            <p>Post by {post.username}</p>
+            <p>Post by {post.username} • {likes} likes</p>
+            <button onClick={() => { setLikes(likes + 1)}}>&hearts;</button>
             <p>{post.content}</p>
         </div>
     )
